5b855de658dd27f3d0282205d2865cdecc5e5962
wuhengqiang <wuhq5@asiainfo.com>
2018-12-05 14:27:24 +0800

    属性访问控制系统2018/12/5

---
+++ src/main/resources/static/admin/js/login.js
@@ -0,0 +1,48 @@
+
+layui.use(['form','layer'],function(){
+    var form = layui.form,
+		layer = parent.layer === undefined ? layui.layer : parent.layer;
+    	$ = layui.jquery;
+
+	//登录按钮事件
+	form.on("submit(login)", function(data){
+        var loadIndex = layer.load(2, {shade: [0.3, '#333']});
+        if($('form').find('input[type="checkbox"]')[0].checked){
+            data.field.rememberMe = true;
+        }else{
+            data.field.rememberMe = false;
+        }
+        $.post(data.form.action, data.field, function(res) {
+            layer.close(loadIndex);
+            if(res.success){
+                location.href="/"+res.url;
+            }else{
+                layer.msg(res.message);
+                $("#randImage").click();
+            }
+        });
+        return false;
+	});
+    $(document).on('keydown', function() {
+        if(event.keyCode == 13) {
+            $(".login_btn").click();
+        }
+    });
+
+    form.on('select(selected)', function(data){
+
+        $.post("/admin/request", {id:data.value}, function(res) {
+            // layer.close(loadIndex);
+            if(res.success){
+                layer.msg(res.role.id);
+                layer.msg(res.message);
+            }else{
+                layer.msg(res.message);
+            }
+        },'json');
+    });
+
+    $("#randImage").click(function () {
+		this.src = "/getCaptcha?t=" + Math.random();
+	});
+});
